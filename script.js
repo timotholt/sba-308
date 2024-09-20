76873d1b511a3119a14670817bb86ed94aa8b081
Timothy Otholt <timotholt@gmail.com>
2024-09-20 09:19:35 -0700

    a save point just for backup purposes

--- script.js
+++ script.js
@@ -104,37 +104,107 @@ const CourseInfo = {
 
 //=======================================================================================
 // helper functions
-//
-// verifyCourseId = Search list of courses to make sure that the courseId is in the list
 //=======================================================================================
 
-const verifyCourseId = (courseId) => Number(CourseInfo.id) === Number(courseId);
-const verifyDate = (dateString) => !isNaN(Date.parse(dateString));
+// Primatives
+const validDate = (dateString) => !isNaN(Date.parse(dateString));
 const getTimestamp = (dateString) => Date.parse(dateString);
 const getCurrentTimestamp = () => Date.now();
-const nowPastDue = (dateString) => (Date.now() >= Date.parse(dateString));
+const nowPastDue = (dateString) => (getCurrentTimestamp() >= getTimestamp(dateString));
 
-const verifyAssignmentGroupCourseId = (assignmentGroup) => verifyCourseId(assignmentGroup.course_id);
+// This is the start run of the application
+const startTimestamp = getCurrentTimestamp();
+const startDatestamp = new Date(startTimestamp);
+
+//=======================================================================================
+// Application specific helper functions
+//
+// 10% of grade: Use functions to handle repeated tasks.
+//=======================================================================================
+
+// Validate Course ID
+// Long winded (was one line but need to use a try/catch for 5% grade)
+function validCourseId(courseId) {
+  
+  const validResult = Number(CourseInfo.id) === Number(courseId);
+
+  // Use a try/catch for 5% of the grade
+  try {
+    if (!validResult)
+      throw `Course ID ${courseId} isn't in the list of courses`;
+  }
+  catch(error) {
+    console.log(error);
+  }
+  finally {
+    return (validResult);
+  };
+}
+
+// Validate Course ID in the assignment group
+// Long winded (was one line but need to use a try/catch for 5% grade)
+function validAssignmentGroupCourseId(assignmentGroup) {
+  
+  const validResult = validCourseId(assignmentGroup.course_id);
+
+  // Use a try/catch for 5% of the grade
+  try {
+    // Using two if statements is 10% of the grade.  This is if #1
+    if (!validResult)
+      throw `Course ID ${assignmentGroup.course_id} in assignmentGroup isn't in the list of courses`;
+  }
+  catch(error) {
+    console.log(error);
+  }
+  finally {
+    return (validResult);
+  }
+};
+
+// Validate individual assignmements to an ID
+function validAssignmentId (assignmentGroup, assignmentId) {
+
+  let validResult = false;
+
+  // Use two different types of loops for 5% of the grade (for loop is #1)
+  for (let i = 0; i < assignmentGroup.assignments.length; i++) {
+
+    // Debug message
+    console.log(`comparing ${Number(assignmentGroup.assignments[i].id)} to ${Number(assignmentId)}`);
+
+    // Using two if statements is 10% of the grade.  This is if #2
+    if (Number(assignmentGroup.assignments[i].id) === Number(assignmentId)) {
+      validResult = true;
+
+      // 3% of the grade: Utilize at least one loop control keyword such as break or continue.
+      break;
+    }
+  }
+  
+  return (validResult);
+};
 
 //==================================================================
 // Test block
 //
-// This test block verifies that the helper functions work
+// This test block verifies that the helper functions work.  This
+// block can be commented out.
 //==================================================================
 
-{
+function verifyHelperFunctions() {
   // Verify course ID
   let courseId = '451';
-  console.log(`courseID ${courseId} in list = ${verifyCourseId(courseId)}`);
+  console.log(`courseID ${courseId} in list = ${validCourseId(courseId)}`);
 
   // Verify date
   let dateString1 = "2024-09-12";
   let dateString2 = "2024-09-12 00:00:00";
   let dateString3 = "HELL WORLD";
 
-  console.log(`dateString ${dateString1} is valid = ${verifyDate(dateString1)}, timestamp = ${getTimestamp(dateString1)}, past due = ${nowPastDue(dateString1)}`);
-  console.log(`dateString ${dateString2} is valid = ${verifyDate(dateString2)}, timestamp = ${getTimestamp(dateString2)}, past due = ${nowPastDue(dateString2)}`);
-  console.log(`dateString ${dateString3} is valid = ${verifyDate(dateString3)}, timestamp = ${getTimestamp(dateString3)}, past due = ${nowPastDue(dateString3)}`);
+  // Verify past due function
+  console.log(`dateString ${dateString1} is valid = ${validDate(dateString1)}, timestamp = ${getTimestamp(dateString1)}, past due = ${nowPastDue(dateString1)}`);
+  console.log(`dateString ${dateString2} is valid = ${validDate(dateString2)}, timestamp = ${getTimestamp(dateString2)}, past due = ${nowPastDue(dateString2)}`);
+  console.log(`dateString ${dateString3} is valid = ${validDate(dateString3)}, timestamp = ${getTimestamp(dateString3)}, past due = ${nowPastDue(dateString3)}`);
 
   // Verify course_id in Assignments
   const assignment1 = {
@@ -143,9 +213,28 @@ const verifyAssignmentGroupCourseId = (assignmentGroup) => verifyCourseId(assign
   let assignment2 = {
     course_id: `451`
   };
-  console.log(`courseID ${assignment1.course_id} is valid = ${verifyAssignmentGroupCourseId(assignment1)}`);
-  console.log(`courseID ${assignment2.course_id} is valid = ${verifyAssignmentGroupCourseId(assignment2)}`);
+  console.log(`courseID ${assignment1.course_id} is valid = ${validAssignmentGroupCourseId(assignment1)}`);
+  console.log(`courseID ${assignment2.course_id} is valid = ${validAssignmentGroupCourseId(assignment2)}`);
+
+  // Verify an assignment # is in the assignment group
+  for (let i = -2; i < 6; i++)
+  console.log(`assignment id ${i} is valid = ${validAssignmentId(AssignmentGroup, i)}`);
+}
+
+//===================================
+// Actual code
+//===================================
+{
+  // verifyHelperFunctions();
+
+  // Display the start time of this batch
+  console.log(`Batch run started at timestamp = ${startDatestamp} (${startTimestamp} Unix time)`);
 
 }
+
+
+//===================================
+// We are done!
+//===================================
 
 console.log(`Goodbye from script.js`);
